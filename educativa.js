ecca88bd56ed66a207400f56ad3a37bada88db97
madalena49 <madalenagomes-ro@hotmail.com>
2026-06-16 21:15:24 -0400

    Add files via upload
    Projeto completo

---
+++ educativa.js
@@ -0,0 +1,27 @@
+function tabuada(){
+
+let numero =
+document.getElementById("numero").value;
+
+let resultado = "";
+
+for(let i=1;i<=10;i++){
+
+resultado +=
+numero + " x " + i + " = " +
+(numero*i) + "<br>";
+
+}
+
+document.getElementById("resultado").innerHTML =
+resultado;
+
+}
+
+function limpar(){
+
+document.getElementById("numero").value = "";
+
+document.getElementById("resultado").innerHTML = "";
+
+}
